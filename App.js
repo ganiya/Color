560fe53e604a15364f74209e7b17a8c1b41d5d7a
Macbook <macbook@Macbooks-MacBook-Air.local>
2020-10-15 20:53:53 -0400

    Collecting Colors Input

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { StyleSheet, FlatList, Alert } from "react-native";
+import ColorButton from "./components/ColorButton";
+import ColorForm from "./components/ColorForm";
+
+import defaultColors from "./data/defaultColors.json";
+
+export default function App() {
+  const [backgroundColor, setBackgroundColor] = useState(
+    "blue"
+  );
+  return (
+    <>
+      <ColorForm onNewColor= {newColor => Alert.alert(`TODO: add color ${newColor}`)} />
+      
+      <FlatList
+        style={[styles.container, { backgroundColor }]}
+        data={defaultColors}
+        renderItem={({ item }) => {
+          return (
+            <ColorButton
+              key={item.id}
+              backgroundColor={item.color}
+              onPress={setBackgroundColor}
+            />
+          );
+        }}
+      />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex"
+  }
+});
---
+++ components/ColorForm.js
@@ -0,0 +1,43 @@
+import React, { useState, useRef } from "react";
+import { View, StyleSheet, TextInput, Button } from "react-native";
+
+export default function ColorForm({ onNewColor = f => f }) {
+    const [inputValue, setValue] = useState("");
+    const input = useRef();
+    return (
+        <View style={styles.container} >
+            <TextInput
+                ref={input}
+                style={styles.txtInput}
+                value={inputValue}
+                onChangeText={setValue}
+                autoCapitalize="none"
+                placeholder="enter a color.."
+            />
+            <Button
+                title="add"
+                onPress={() => {
+                    input.current.blur();
+                    onNewColor(inputValue);
+                    setValue("");
+                }}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 40,
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    txtInput: {
+        flex: 1,
+        borderWidth: 2,
+        fontSize: 20,
+        margin: 5,
+        borderRadius: 5,
+        padding: 5
+    }
+});
