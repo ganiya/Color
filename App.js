c1fecbdbf799aef40c0cb2bac9f22192bae4f99e
Macbook <macbook@Macbooks-MacBook-Air.local>
2020-10-15 21:41:40 -0400

    custom hooks

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
-import { StyleSheet, FlatList, Alert } from "react-native";
+import { StyleSheet, FlatList } from "react-native";
 import ColorButton from "./components/ColorButton";
 import ColorForm from "./components/ColorForm";
+import {generate} from "shortid";
 
 import defaultColors from "./data/defaultColors.json";
 
+const useColors = () => {
+const [colors, setColors]= useState([]);  
+const addColor = color => {
+    const newColor = {id:generate (), color}
+    setColors([newColor,...colors]);
+  };
+  return { colors, addColor};
+};
+
 export default function App() {
-  const [backgroundColor, setBackgroundColor] = useState(
-    "blue"
-  );
-  return (
+  const [backgroundColor, setBackgroundColor] = useState("blue");
+    const { colors,addColor} = useColors();
+
+     return (
     <>
-      <ColorForm onNewColor= {newColor => Alert.alert(`TODO: add color ${newColor}`)} />
+      <ColorForm onNewColor= {addColor } />
       
       <FlatList
         style={[styles.container, { backgroundColor }]}
-        data={defaultColors}
+        data={colors}
         renderItem={({ item }) => {
           return (
             <ColorButton
