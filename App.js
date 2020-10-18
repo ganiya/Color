b1fe5b8727452745e6da6dcc39dcd5e8ea5b3a43
Macbook <macbook@Macbooks-MacBook-Air.local>
2020-10-18 19:56:34 -0400

    navigation

--- App.js
+++ App.js
@@ -1,48 +1,18 @@
-import React, { useState } from "react";
-import { StyleSheet, FlatList } from "react-native";
-import ColorButton from "./components/ColorButton";
-import ColorForm from "./components/ColorForm";
-import {generate} from "shortid";
+import React from "react";
+import ColorList from "./components/ColorList";
+import ColorDetails from "./components/ColorDetails";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import defaultColors from "./data/defaultColors.json";
-
-const useColors = () => {
-const [colors, setColors]= useState([]);  
-const addColor = color => {
-    const newColor = {id:generate (), color}
-    setColors([newColor,...colors]);
-  };
-  return { colors, addColor};
-};
+const { Navigator, Screen } = createStackNavigator();
 
 export default function App() {
-  const [backgroundColor, setBackgroundColor] = useState("blue");
-    const { colors,addColor} = useColors();
-
-     return (
-    <>
-      <ColorForm onNewColor= {addColor } />
-      
-      <FlatList
-        style={[styles.container, { backgroundColor }]}
-        data={colors}
-        renderItem={({ item }) => {
-          return (
-            <ColorButton
-              key={item.id}
-              backgroundColor={item.color}
-              onPress={setBackgroundColor}
-            />
-          );
-        }}
-      />
-    </>
+  return (
+    <NavigationContainer>
+      <Navigator>
+        <Screen name="Home" component={ColorList} />
+        <Screen name="Details" component={ColorDetails} />
+      </Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: "flex"
-  }
-});
